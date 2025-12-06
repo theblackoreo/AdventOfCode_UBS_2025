7f0606b96c36da91af95657ad52eeadbeb6ab496
theblackoreo <fabio.tagliani03@universitadipavia.it>
2025-12-06 13:08:33 +0100

    day 2

---
+++ Day2/day2_1.py
@@ -0,0 +1,25 @@
+total = 0
+
+with open("day2_1_data.txt", "r") as f:
+    for line in f:
+        parts = line.strip().split(",")
+        
+        for ran in parts:
+            start, end = map(int, ran.split("-"))
+
+            for id in range(start, end + 1):
+                id_str = str(id)
+                if len(id_str) % 2 == 0:
+                    
+                    middle = int(len(id_str)/2)
+                    m = 0
+                    while m < len(id_str)/2:
+                        if id_str[m] != id_str[middle]:
+                            break
+                        m += 1
+                        middle += 1
+                    else:
+                        total += id
+
+
+print(total)
---
+++ Day2/day2_2.py
@@ -0,0 +1,22 @@
+total = 0
+
+with open("day2_1_data.txt", "r") as f:
+    for line in f:
+        parts = line.strip().split(",")
+        
+        for ran in parts:
+            start, end = map(int, ran.split("-"))
+
+            for id in range(start, end + 1):
+                s = str(id)
+                n = len(s)
+
+                for size in range(1, n // 2 + 1):
+                    if n % size != 0:
+                        continue
+                    pattern = s[:size]
+                    if pattern * (n // size) == s:
+                        total += id
+                        break 
+
+print(total)
